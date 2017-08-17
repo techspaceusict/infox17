a419beefbcdce35158fc0f55847a5bd003f756de
Naveen yadav <yadavnaveen826@gmail.com>
2017-08-17 21:45:24 +0530

    Added venue to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808172352) do
+ActiveRecord::Schema.define(version: 20170817161148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20170808172352) do
     t.string   "time"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "venue"
   end
 
   create_table "queries", force: :cascade do |t|
