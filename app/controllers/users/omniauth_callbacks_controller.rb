3077d0c24644577f2d83ae9bba9078b847f9e79a
Naveen yadav <yadavnaveen826@gmail.com>
2017-08-26 19:38:37 +0530

    Cleaning the code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
